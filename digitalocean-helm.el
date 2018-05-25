161f21b29927c33b685fbd4ab1f36193a18bd049
Oliver Marks <oly@digitaloctave.com>
2018-05-25 22:26:36 +0100

    Add requirement on digitalocean plugin

--- digitalocean-helm.el
+++ digitalocean-helm.el
@@ -3,11 +3,11 @@
 ;; Copyright (C) 2018  Oliver Marks
 
 ;; Author: Oliver Marks <oly@digitaloctave.com>
-;; URL: https://github.com/olymk2/digitalocean-api
+;; URL: https://gitlab.com/olymk2/digitalocean-api
 ;; Keywords: Processes tools
 ;; Version: 0.1
 ;; Created 01 July 2018
-;; Package-Requires: ((emacs "24.3")(helm "2.5"))
+;; Package-Requires: ((emacs "24.3")(helm "2.5")(digitalocean "0.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@
 
 ;;; Code:
 
-(defvar helm-digitalocean-droplet-source
+(defvar digitalocean-helm-droplet-source
   ;; Helm droplet list and actions
   '((name . "Digitalocean Droplets")
     (candidates . do/digitalocean-droplet-list)
@@ -68,46 +68,57 @@
 		   (do/get-droplet-id-from-candidate candidate)
                    "destroy")))))))
 
-(defun do/helm-digitalocean-droplets ()
-  ;; Show helm droplet list
+;;;autoload
+(defun digitalocean-helm-droplets ()
+  "Show helm droplet list."
   (interactive)
   (helm :sources '(helm-digitalocean-droplet-source)))
 
 
-(defvar helm-digitalocean-image-source
+(defvar digitalocean-helm-image-source
   ;; Helm image list and actions
   '((name . "Digitalocean Images")
-    (candidates . do/digitalocean-images-list)
+    (candidates . digitalocean-helm-digitalocean-images-list)
     (action . (("Test" . (lambda (candidate)
 			   (message-box
 			    "selected: %s"
 			    (helm-marked-candidates))))))))
 
-(defun do/helm-digitalocean-images ()
-  ;; Show helm image list
+;;;autoload
+(defun digitalocean-helm-images ()
+  "Show helm image list."
   (interactive)
   (helm :sources '(helm-digitalocean-image-source)))
 
-(defvar helm-digitalocean-region-source
+(defvar digitalocean-helm-region-source
   ;; Helm region list and actioons
   '((name . "Digitalocean Regions")
-    (candidates . do/digitalocean-regions-list)
+    (candidates . digitalocean-helm-digitalocean-regions-list)
     (action . (("Test" . (lambda (candidate)
 			   (message-box
 			    "selected: %s"
 			    (helm-marked-candidates))))))))
 
-(defun do/helm-digitalocean-regions ()
-  ;; Show helm region list
+;;;autoload
+(defun digitalocean-helm-regions ()
+  "Show helm region list."
   (interactive)
   (helm :sources '(helm-digitalocean-region-source)))
 
-(defun do/get-droplet-id-from-candidate (candidate)
-  ;; Give a helm droplet candidate get the droplets id
+
+(defun digitalocean-helm-build-ssh-path (candidate dir)
+  "Give a helm CANDIDATE & DIR return a tramp ssh path."
+  (format "/ssh:root@%s:%s"
+	  (digitalocean-get-droplet-ip4-from-id
+	   (digitalocean-helm-get-droplet-id-from-candidate candidate)) dir))
+
+
+(defun digitalocean-helm-get-droplet-id-from-candidate (candidate)
+  "Give a helm droplet CANDIDATE get the droplets id."
   (number-to-string (cdr (assoc 'id candidate))))
 
-(defun do/get-droplet-name-from-candidate (candidate)
-  ;; Give a helm droplet candidate get the droplets name
+(defun digitalocean-helm-get-droplet-name-from-candidate (candidate)
+  "Give a helm droplet CANDIDATE get the droplets name."
   (cdr (assoc 'name candidate)))
 
 ;;; (Features)
